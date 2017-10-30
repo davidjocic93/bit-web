2de05465e96a9f8ae50cfbc5068aecf3c6c34acd
davidjocic93 <davidjocic93@gmail.com>
2017-10-30 21:52:16 +0100

    fly you motherfucker

--- mario/script.js
+++ mario/script.js
@@ -31,3 +31,34 @@ bodySelected.addEventListener("keyup", function (event) {
     }
 });
 
+bodySelected.addEventListener("keydown", function (event) {
+
+    if (event.keyCode === 38) {
+        mario.style.display = "none";
+        marioHidden.style.display = "block";
+        marioHidden.style.position = "relative";
+        marioHidden.style.bottom = 50 + "px";
+    }
+
+    bodySelected.style.backgroundPositionX = 0 + "px";
+    
+            function moveX() {
+                bodySelected.style.backgroundPositionX = -10 + "px";
+            }
+    
+            (function moving() {
+                     setInterval(moveX, 300); 
+            })();
+
+});
+
+bodySelected.addEventListener("keyup", function (event) {
+    
+    if (event.keyCode === 38) {
+        mario.style.display = "block";
+        marioHidden.style.display = "none";
+        marioHidden.style.position = "relative";
+        marioHidden.style.bottom = 0 + "px";
+    }
+    });
+
